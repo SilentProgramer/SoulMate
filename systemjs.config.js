e04d221f1edbeeb707f2670aa15be6448f28b626
Hemant <hemantsinghsrm@gmail.com>
2016-06-12 14:20:36 +0530

    Heroku still not working. Trying something

--- systemjs.config.js
+++ systemjs.config.js
@@ -9,7 +9,7 @@
     '@angular':                   'scripts/@angular',
     'angular2-in-memory-web-api': 'scripts/angular2-in-memory-web-api',
     'rxjs':                       'scripts/rxjs',
-    'zone.js':                    'scripts/zone.js',
+    //'zone.js':                    'scripts/zone.js',
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
